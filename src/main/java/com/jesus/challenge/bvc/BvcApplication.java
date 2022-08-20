f141dc0cca3a9984a62bdf4724a27e5d8b065148
Jesus Marin <jesusmarin@outlook.es>
2022-08-20 09:19:46 -0500

    start py bvc

---
+++ src/main/java/com/jesus/challenge/bvc/BvcApplication.java
@@ -0,0 +1,13 @@
+package com.jesus.challenge.bvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jesus/challenge/bvc/BvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesus.challenge.bvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
